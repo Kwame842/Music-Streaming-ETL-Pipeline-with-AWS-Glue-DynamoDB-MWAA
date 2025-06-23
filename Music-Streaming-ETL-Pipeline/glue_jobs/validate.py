39c8d44174e0ac86afba09b80965800ccce024e7
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-06-23 20:44:18 +0000

    S3 configuration

--- Music-Streaming-ETL-Pipeline/glue_jobs/validate.py
+++ Music-Streaming-ETL-Pipeline/glue_jobs/validate.py
@@ -2,3 +2,9 @@ import pandas as pd
 import boto3
 import io
 import sys
+
+# S3 config
+raw_base = "s3://music-streaming-data-02/raw"
+validated_base = "s3://music-streaming-data-02/validated"
+bad_base = "s3://music-streaming-data-02/bad-records"
+bucket = "music-streaming-data-02"
