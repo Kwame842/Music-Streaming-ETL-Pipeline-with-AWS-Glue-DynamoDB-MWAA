87b453bb0f3983bbf7019ce1cafd72616f2d0589
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-06-23 20:52:15 +0000

    Configurations

--- Music-Streaming-ETL-Pipeline/glue_jobs/load_dynamo.py
+++ Music-Streaming-ETL-Pipeline/glue_jobs/load_dynamo.py
@@ -6,3 +6,14 @@ import pyarrow.fs as fs
 import posixpath
 import re
 from botocore.exceptions import ClientError
+
+
+# Logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Configuration
+REGION = os.environ.get("AWS_REGION", "us-east-1")
+S3_BUCKET = "music-streaming-data-02"
+BASE_PREFIX = "processed"
+s3_fs = fs.S3FileSystem(region=REGION)
