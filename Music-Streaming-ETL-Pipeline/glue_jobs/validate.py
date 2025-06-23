8ea0d6b09b2e8db80a353c144bb68e8f82e3ad31
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-06-23 20:44:59 +0000

    Definition of required columns

--- Music-Streaming-ETL-Pipeline/glue_jobs/validate.py
+++ Music-Streaming-ETL-Pipeline/glue_jobs/validate.py
@@ -8,3 +8,10 @@ raw_base = "s3://music-streaming-data-02/raw"
 validated_base = "s3://music-streaming-data-02/validated"
 bad_base = "s3://music-streaming-data-02/bad-records"
 bucket = "music-streaming-data-02"
+
+# Required columns
+required_users = ['user_id', 'user_name', 'user_age', 'user_country', 'created_at']
+required_songs = ['track_id', 'track_name', 'track_genre', 'duration_ms']
+required_streams = ['user_id', 'track_id', 'listen_time']
+
+s3 = boto3.client("s3")
