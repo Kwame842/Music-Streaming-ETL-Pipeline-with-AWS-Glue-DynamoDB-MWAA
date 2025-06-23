f863249ab262b145ee477c65a64084c38b349d0d
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-06-23 20:37:05 +0000

    dags packages import

---
+++ Music-Streaming-ETL-Pipeline/dags/music_etl_pipeline.py
@@ -0,0 +1,8 @@
+from airflow import DAG
+from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
+from airflow.exceptions import AirflowSkipException
+import boto3
+import datetime
