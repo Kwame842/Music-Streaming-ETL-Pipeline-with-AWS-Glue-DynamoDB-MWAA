1dc4406b1fea92ade2403379becb22c68a11663a
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-06-23 20:47:53 +0000

    Setup for SparkContext

--- Music-Streaming-ETL-Pipeline/glue_jobs/transform.py
+++ Music-Streaming-ETL-Pipeline/glue_jobs/transform.py
@@ -4,3 +4,7 @@ from awsglue.transforms import *
 from pyspark.context import SparkContext
 from pyspark.sql.functions import col, to_date, countDistinct, sum as spark_sum, count, row_number
 from pyspark.sql.window import Window
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
