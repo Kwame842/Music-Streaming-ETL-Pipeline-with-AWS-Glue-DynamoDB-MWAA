09ca2994e96e1ee64a89f222a80df45ada1f66bc
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-06-23 20:50:18 +0000

    Transformations and KPI computations

--- Music-Streaming-ETL-Pipeline/glue_jobs/transform.py
+++ Music-Streaming-ETL-Pipeline/glue_jobs/transform.py
@@ -18,3 +18,38 @@ songs_df = spark.read.option("header", True).csv("s3://music-streaming-data-02/r
 # Preprocessing
 streams_df = streams_df.withColumn("listen_date", to_date("listen_time"))
 songs_df = songs_df.select("track_id", "track_name", "track_genre", "duration_ms")
+
+# Join stream with songs
+joined_df = streams_df.join(songs_df, on="track_id", how="inner")
+
+# KPI Aggregation
+kpi_df = joined_df.groupBy("listen_date", "track_genre").agg(
+    countDistinct("user_id").alias("unique_listeners"),
+    spark_sum("duration_ms").alias("total_listening_time"),
+    count("track_id").alias("listen_count")
+)
+
+# Average Listening Time per User
+avg_df = kpi_df.withColumn(
+    "avg_listen_time_per_user",
+    col("total_listening_time") / col("unique_listeners")
+)
+
+# Top 3 Songs per Genre per Day
+window = Window.partitionBy("listen_date", "track_genre").orderBy(col("listen_count").desc())
+joined_count = joined_df.groupBy("listen_date", "track_genre", "track_name") \
+    .agg(count("*").alias("listen_count"))
+
+top_songs = joined_count.withColumn("rank", row_number().over(window)).filter(col("rank") <= 3)
+
+# Top 5 Genres per Day
+genre_window = Window.partitionBy("listen_date").orderBy(col("listen_count").desc())
+genre_count = joined_df.groupBy("listen_date", "track_genre") \
+    .agg(count("*").alias("listen_count"))
+
+top_genres = genre_count.withColumn("rank", row_number().over(genre_window)).filter(col("rank") <= 5)
+
+# Write outputs to processed path
+avg_df.write.mode("overwrite").parquet("s3://music-streaming-data-02/processed/avg_metrics")
+top_songs.write.mode("overwrite").parquet("s3://music-streaming-data-02/processed/top_songs")
+top_genres.write.mode("overwrite").parquet("s3://music-streaming-data-02/processed/top_genres")
